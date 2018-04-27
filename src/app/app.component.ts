2f4dfe458c1d7f4ea3a87fb22867c31fccc70f34
Aaron <developingaaron@gmail.com>
2018-04-27 23:15:12 +0100

    animations working successfully and placed in separate file. All working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FabAnimations } from './app.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: FabAnimations
 })
 export class AppComponent {
-  title = 'app';
+  animationState = 'inactive';
+
+  onToggleFab() {
+    if (this.animationState === 'inactive') {
+      this.animationState = 'active';
+    } else {
+      this.animationState = 'inactive';
+    }
+  }
 }
